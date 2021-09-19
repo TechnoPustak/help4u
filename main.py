a17fca4c8d28a8be3a313320faac217ee566ead1
TechnoPustak <factsworld1109@gmail.com>
2021-09-19 11:34:51 +0530

    confirm link

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import string
 import random
-from flask import Flask, render_template, request, redirect, session
+from flask_mail import Mail, Message
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_sslify import SSLify
-import smtplib
+from itsdangerous import URLSafeTimedSerializer
+from itsdangerous.exc import SignatureExpired
+# import smtplib
 
 app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'factsworld1109@gmail.com'
+app.config['MAIL_PASSWORD'] = 'pnhnywcnwixzhhie'
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
+mail = Mail(app)
+s = URLSafeTimedSerializer('my-secret')
 sslify = SSLify(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kiympnjddnapxr:c3802988090175e2d6f23d27f2f8ef078d7dfea85a76968cb364d41ea72d7a13@ec2-54-154-101-45.eu-west-1.compute.amazonaws.com/d9h0lkg28n6udh'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,14 +30,13 @@ login_manager.init_app(app)
 def load_user(username):
     return login.query.get(username)
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('factsworld1109@gmail.com', 'pnhnywcnwixzhhie')
-    server.sendmail('factsworld1109@gmail.com', to, content)
-    server.close()
-
+# def sendEmail(to, content):
+#     server = smtplib.SMTP('smtp.gmail.com', 587)
+#     server.ehlo()
+#     server.starttls()
+#     server.login('factsworld1109@gmail.com', 'pnhnywcnwixzhhie')
+#     server.sendmail('factsworld1109@gmail.com', to, content)
+#     server.close()
 
 class login(UserMixin, db.Model):
     def get_id(self):
@@ -63,7 +73,7 @@ def signin():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    msg = None
+    lol = None
     if request.method == 'POST':
         session["username"] = request.form.get('username').lower()
         session["first_name"] = request.form.get('first_name')
@@ -72,50 +82,60 @@ def index():
         rpassword = request.form.get('rpassword')
         session["birthday"] = request.form.get('birthday')
         session["gender"] = request.form.get('gender')
-        session["email"] = request.form.get('email')
+        email = request.form.get('email')
         session["about"]= request.form.get('about')
         sign_user = login.query.filter_by(username=session.get('username')).first()
         sign_email = login.query.filter_by(email=session.get('email')).first()
         if sign_user:
-            msg = 'Username Used.'
+            lol = 'Username Used.'
         elif sign_email:
-            msg = 'Already an account with this email.'
+            lol = 'Already an account with this email.'
         elif session.get('password')!=rpassword:
-            msg='Password and repeat password are different.'
+            lol='Password and repeat password are different.'
         else:
-            N = 7
-            res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N))
-            session['random'] = res
-            return redirect('/verify')
-    return render_template('index.html', msg=msg, title='Sign up')
+            token = s.dumps(email, salt='email-confirm')
+            # N = 7
+            # res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N))
+            # session['random'] = res
+            msg = Message('Confirm Email', sender='factsworld1109@gmail.com', recipients=[email])
+            link = url_for('verify', token=token, external=True)
+            msg.body = 'Your link is {}'.format(link)
+            mail.send(msg)
+            return link
+    return render_template('index.html', msg=lol, title='Sign up')
 
-@app.route("/verify", methods=["GET", "POST"])
-def verify():
-    to = session.get('email')
-    content = f'Your verification code is {session.get("random")}.'
-    sendEmail(to, content)
-    if request.method == 'POST':
-        code = request.form.get('code')
-        if code == session['random']:
-            username = session.get('username')
-            first_name = session.get('first_name')
-            last_name = session.get('last_name')
-            password = session.get('password')
-            birthday = session.get('birthday')
-            gender = session.get('gender')
-            email = session.get('email')
-            about = session.get('about')
-            info = login(username=username, first_name=first_name, last_name=last_name, password=password, birthday=birthday, gender=gender, email=email, about=about)
-            try:
-                db.session.add(info)
-                db.session.commit()
-                session.pop('random')
-                return 'Added'
-            except:
-                return 'Something went wrong your credentials not uploaded.'
-        else:
-            return 'Wrong'
-    return render_template('verify.html', title='Email verification', email=session.get('email'))
+@app.route("/verify/<token>")
+def verify(token):
+    # to = session.get('email')
+    # content = f'Your verification code is {session.get("random")}.'
+    # sendEmail(to, content)
+    # if request.method == 'POST':
+    #     code = request.form.get('code')
+    #     if code == session['random']:
+    #         username = session.get('username')
+    #         first_name = session.get('first_name')
+    #         last_name = session.get('last_name')
+    #         password = session.get('password')
+    #         birthday = session.get('birthday')
+    #         gender = session.get('gender')
+    #         email = session.get('email')
+    #         about = session.get('about')
+    #         info = login(username=username, first_name=first_name, last_name=last_name, password=password, birthday=birthday, gender=gender, email=email, about=about)
+    #         try:
+    #             db.session.add(info)
+    #             db.session.commit()
+    #             session.pop('random')
+    #             return 'Added'
+    #         except:
+    #             return 'Something went wrong your credentials not uploaded.'
+    #     else:
+    #         return 'Wrong'
+    # return render_template('verify.html', title='Email verification', email=session.get('email'))
+    try:
+        email = s.loads(token, salt='email-confirm', max_age=3600)
+        return True
+    except SignatureExpired:
+        return False
 
 @app.route("/forgot-password", methods=["GET", "POST"])
 def passwordemail():
