28c1d18c82e38ac2e79e6a88bf49d05e0038091d
TechnoPustak <73098888+TechnoPustak@users.noreply.github.com>
2021-12-28 20:46:21 +0530

    some frontend changing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask_mail import Mail, Message
-import json, time, convertcode
+import json
+import time
+import convertcode
 from flask import Flask, render_template, request, session, url_for, flash, redirect
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
@@ -11,10 +13,12 @@ with open('config.json', 'r') as c:
     params = json.load(c)["params"]
     c.seek(0)
     database = json.load(c)["database"]
+    c.seek(0)
+    statics = json.load(c)["statics"]
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = params['email']
 app.config['MAIL_PASSWORD'] = params['email-password']
@@ -25,6 +29,7 @@ mail = Mail(app)
 s = URLSafeTimedSerializer('my-secret')
 sslify = SSLify(app)
 
+# heroku pg:psql postgresql-spherical-80201 --app help4you
 app.config['SQLALCHEMY_DATABASE_URI'] = f"{database['app']}://{database['user']}:{database['password']}@{database['host']}/{database['database']}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "super-secret-key"
@@ -33,9 +38,11 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_user(username):
     return login.query.get(username)
+
 
 class login(UserMixin, db.Model):
     def get_id(self):
@@ -49,6 +56,7 @@ class login(UserMixin, db.Model):
     email = db.Column(db.String(100), nullable=False)
     profile_pic = db.Column(db.String(), nullable=False)
 
+
 class posts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     post = db.Column(db.String(50000), nullable=False)
@@ -57,6 +65,7 @@ class posts(db.Model):
     time = db.Column(db.String(100), nullable=False)
     grade = db.Column(db.String(100), nullable=False)
 
+
 class answers(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     answer = db.Column(db.String(50000), nullable=False)
@@ -64,9 +73,11 @@ class answers(db.Model):
     question_id = db.Column(db.String(100), nullable=False)
     time = db.Column(db.String(100), nullable=False)
 
+
 @app.errorhandler(401)
 def unauthorized(e):
     return redirect('/login')
+
 
 @app.route("/login", methods=["GET", "POST"])
 def signin():
@@ -79,13 +90,14 @@ def signin():
         if user and convertcode.decodecode(user.password) == password:
             login_user(user, remember=True)
             return redirect('/dashboard')
-        elif user1 and convertcode.decodecode(user1.password)== password:
+        elif user1 and convertcode.decodecode(user1.password) == password:
             login_user(user1, remember=True)
             return redirect('/dashboard')
         else:
             flash('Entered Credentials are wrong.', 'danger')
             return render_template('login.html', title='Login', params=params)
     return render_template('login.html', title='Login', params=params)
+
 
 @app.route("/", methods=["GET", "POST"])
 def signup():
@@ -98,38 +110,44 @@ def signup():
         session["birthday"] = request.form.get('birthday')
         session["gender"] = request.form.get('gender')
         email = request.form.get('email')
-        sign_user = login.query.filter_by(username=session.get('username')).first()
+        sign_user = login.query.filter_by(
+            username=session.get('username')).first()
         sign_email = login.query.filter_by(email=email).first()
         if sign_user:
             flash('Username Used.')
         elif sign_email:
             flash('Already an account with this email.')
-        elif session.get('password')!=rpassword:
+        elif session.get('password') != rpassword:
             flash('Password and repeat password are different.')
         else:
             token = s.dumps(email, salt='email-confirm')
-            msg = Message('Confirm Email', sender='factsworld1109@gmail.com', recipients=[email])
+            msg = Message(
+                'Confirm Email', sender='factsworld1109@gmail.com', recipients=[email])
             link = url_for('verify', token=token, _external=True)
-            msg.body = 'Your link is {} but go on this link from the same device and browser used for registration.'.format(link)
+            msg.body = 'Your link is {} but go on this link from the same device and browser used for registration.'.format(
+                link)
             mail.send(msg)
             return render_template('box.html', email=email)
-    return render_template('signup.html', title = f"{params['title']}: Sign Up for a new account", params=params)
+    return render_template('signup.html', title=f"{params['title']}: Sign Up for a new account", params=params)
+
 
 @app.route('/dashboard', methods=["GET", "POST"])
 @login_required
 def dashboard():
     questions = posts.query.filter_by().all()
     users = login.query.filter_by().all()
-    if request.method=='POST':
+    if request.method == 'POST':
         username = current_user.username
         post = request.form.get('question')
         subject = request.form.get('subject')
         grade = request.form.get('grade')
-        info = posts(username=username, post=post, subject=subject, time=time.time(), grade=grade)
+        info = posts(username=username, post=post,
+                     subject=subject, time=time.time(), grade=grade)
         db.session.add(info)
         db.session.commit()
         return redirect('/dashboard')
-    return render_template("dashboard.html",title=f"{params['title']}: Ask and Answer Questions",questions=questions,users=users,time=time.time())
+    return render_template("dashboard.html", title=f"{params['title']}: Ask and Answer Questions", questions=questions, users=users, time=time.time())
+
 
 @app.route('/answer/<int:sno>', methods=["GET", "POST"])
 @login_required
@@ -137,16 +155,18 @@ def answer(sno):
     users = login.query.filter_by().all()
     question = posts.query.filter_by(sno=sno).first()
     myanswers = answers.query.filter_by(question_id=str(sno)).all()
-    if request.method =='POST':
+    if request.method == 'POST':
         myanswer = request.form.get('answer')
-        answer_info = answers(answer=myanswer, username=current_user.username, question_id=sno, time=time.time())
+        answer_info = answers(
+            answer=myanswer, username=current_user.username, question_id=sno, time=time.time())
         db.session.add(answer_info)
         db.session.commit()
         return redirect('/answer/'+str(sno))
     if question:
-        return render_template('answer.html',title=f"{params['title']}: Give Answers", question=question, time=time.time(), users=users, sno=sno, answers=myanswers)
+        return render_template('answer.html', title=f"{params['title']}: Give Answers", question=question, time=time.time(), users=users, sno=sno, answers=myanswers)
     else:
         return redirect('/dashboard')
+
 
 @app.route("/verify/<token>")
 def verify(token):
@@ -159,8 +179,9 @@ def verify(token):
         birthday = session.get('birthday')
         gender = session.get('gender')
         encpass = convertcode.convertcode(password)
-        profile_pic = 'https://help4you.herokuapp.com/static/profile.png'
-        info = login(username=username, first_name=first_name, last_name=last_name, password=encpass, birthday=birthday, gender=gender, email=email, profile_pic=profile_pic)
+        profile_pic = statics['profile_pic']
+        info = login(username=username, first_name=first_name, last_name=last_name, password=encpass,
+                     birthday=birthday, gender=gender, email=email, profile_pic=profile_pic)
         db.session.add(info)
         db.session.commit()
         flash('Your account has been created successfully.', 'success')
@@ -170,17 +191,20 @@ def verify(token):
     except BadSignature:
         return 'No such link.'
 
+
 @app.route("/forgot-password", methods=["GET", "POST"])
 def passwordemail():
     if request.method == 'POST':
         email = request.form.get('code')
         token = s.dumps(email, salt='change-pass')
-        msg = Message('Password Change', sender='factsworld1109@gmail.com', recipients=[email])
+        msg = Message('Password Change',
+                      sender='factsworld1109@gmail.com', recipients=[email])
         link = url_for('changepass', token=token, _external=True)
         msg.body = 'Your link is {}'.format(link)
         mail.send(msg)
         return render_template('box.html', email=email)
     return render_template('password.html', title='Password Recovery')
+
 
 @app.route('/change-password/<token>')
 def changepass(token):
@@ -191,9 +215,11 @@ def changepass(token):
     except SignatureExpired:
         return 'False'
 
+
 @app.route("/google4a74fd24a326259f.html")
 def googlesearchverify():
     return render_template('google4a74fd24a326259f.html')
+
 
 @app.route('/logout')
 @login_required
@@ -201,6 +227,7 @@ def logout():
     logout_user()
     flash('You has been logged out successfully. ', 'success')
     return redirect('/login')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
